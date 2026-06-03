92f7aaf0280efeeb84ca88f5f7464076073b85f8
kulonglam <makhol1990@gmail.com>
2026-06-03 11:48:03 +0300

    Updated allowed hosts

--- avvento_hrmis/settings.py
+++ avvento_hrmis/settings.py
@@ -31,7 +31,7 @@ DEBUG =os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [ 
     'localhost',
     '127.0.0.1',
-    'https://human-resource-management-system-i1d3.onrender.com']
+    'human-resource-management-system-i1d3.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
      'https://human-resource-management-system-i1d3.onrender.com'
